945251eee838b99effb276913c1da75c9110bb0b
Ramazan Demir <ramazan.demir@sahibinden.com>
2018-03-18 16:32:47 +0300

    fix number

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,6 @@
 INSERT INTO academician (name, surname, username,department,faculty,password) VALUES ('Yrd.Doç.Dr.Pınar', 'Onay Durdu', 'ponaydurdu','Endüstri_Mühendisliği','Mühendislik','1');
 INSERT INTO academician (name, surname, username,department,faculty,password) VALUES ('Yrd.Doç.Dr.Burak', 'Inner', 'binner','Makine_Mühendisliği','Mühendislik','1');
 INSERT INTO academician (name, surname, username,department,faculty,password) VALUES ('Doç.Dr.Ahmet', 'Sayar', 'asayar','Bilgisiyar_Mühendisliği','Mühendislik','1');
-INSERT INTO student (name, surname,mac_id,number,password) VALUES ('Umut', 'Soysal','RZ5G54K6GX4SYNF2C','140202042','1');
+INSERT INTO student (name, surname,mac_id,number,password) VALUES ('Umut', 'Soysal','RZ5G54K6GX4SYNF2C','140202040','1');
 INSERT INTO student (name, surname,mac_id,number,password) VALUES ('Ramazan', 'Demir','HG95QJ2J862FJKVE5','140202092','1');
-INSERT INTO student (name, surname,mac_id,number,password) VALUES ('İsmail', 'Reşat','5Y7VRV54MUFS9NHXQ','140202040','1');
+INSERT INTO student (name, surname,mac_id,number,password) VALUES ('İsmail', 'Reşat','5Y7VRV54MUFS9NHXQ','140202042','1');
